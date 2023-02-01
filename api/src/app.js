27d933f203fb1708449728190bd243f00d3f7fef
Code-Zeal <codeseazeal@gmail.com>
2023-02-01 18:26:40 -0400

    URL_FRONT

--- api/src/app.js
+++ api/src/app.js
@@ -3,6 +3,8 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
+require("dotenv").config();
+const { URL_FRONT } = process.env;
 
 require("./db.js");
 
@@ -15,10 +17,7 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://pi-foods-soy-henry-hqeg.vercel.app"
-  ); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", URL_FRONT); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
